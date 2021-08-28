5acc2b7da10393521930fe3777c78622c063bce8
Marcin Wosinek <marcin@wosinek.eu>
2021-08-28 11:09:45 +0200

    add hello world to the html

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
 const header = document.createElement("div");
 
 header.innerHTML = "hello world";
 
 document.body.appendChild(header);
+
+/******/ })()
+;
